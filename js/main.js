08d28466f046b154baf73797b3fd1eda9d261224
Noctis <noctis@NoctisdeMacBook-Air.local>
2018-07-09 21:24:04 +0800

    2018-7-9

--- js/main.js
+++ js/main.js
@@ -1,283 +1,342 @@
-jQuery(document).ready(function($){
-	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
-	var MQL = 1170;
-
-	//primary navigation slide-in effect
-	if($(window).width() > MQL) {
-		var headerHeight = $('.cd-header').height();
-		$(window).on('scroll',
-		{
-	        previousTop: 0
-	    }, 
-	    function () {
-		    var currentTop = $(window).scrollTop();
-		    //check if user is scrolling up
-		    if (currentTop < this.previousTop ) {
-		    	//if scrolling up...
-		    	if (currentTop > 0 && $('.cd-header').hasClass('is-fixed')) {
-		    		$('.cd-header').addClass('is-visible');
-		    	} else {
-		    		$('.cd-header').removeClass('is-visible is-fixed');
-		    	}
-		    } else {
-		    	//if scrolling down...
-		    	$('.cd-header').removeClass('is-visible');
-		    	if( currentTop > headerHeight && !$('.cd-header').hasClass('is-fixed')) $('.cd-header').addClass('is-fixed');
-		    }
-		    this.previousTop = currentTop;
-		});
-	}
-
-	//open/close primary navigation
-	$('.cd-primary-nav-trigger').on('click', function(){
-		$('.cd-menu-icon').toggleClass('is-clicked'); 
-		$('.cd-header').toggleClass('menu-is-open');
-		
-		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
-		if( $('.cd-primary-nav').hasClass('is-visible') ) {
-			$('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
-				$('body').removeClass('overflow-hidden');
-			});
-		} else {
-			$('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
-				$('body').addClass('overflow-hidden');
-			});	
+function styleDefault(){
+	$('#particles-js').css("background-color", "rgb(0,0,0)");
+	particlesJS("particles-js", {
+		"particles": {
+		"number": {
+			"value": 80,
+			"density": {
+			"enable": true,
+			"value_area": 800
+			}
+		},
+		"color": {
+			"value": "#ffffff"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+			"width": 0,
+			"color": "#000000"
+			},
+			"polygon": {
+			"nb_sides": 5
+			},
+			"image": {
+			"src": "img/github.svg",
+			"width": 100,
+			"height": 100
+			}
+		},
+		"opacity": {
+			"value": 0.5,
+			"random": false,
+			"anim": {
+			"enable": false,
+			"speed": 1,
+			"opacity_min": 0.1,
+			"sync": false
+			}
+		},
+		"size": {
+			"value": 3,
+			"random": true,
+			"anim": {
+			"enable": false,
+			"speed": 40,
+			"size_min": 0.1,
+			"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": true,
+			"distance": 150,
+			"color": "#ffffff",
+			"opacity": 0.4,
+			"width": 1
+		},
+		"move": {
+			"enable": true,
+			"speed": 6,
+			"direction": "none",
+			"random": false,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+			"enable": false,
+			"rotateX": 600,
+			"rotateY": 1200
+			}
 		}
-	});
-});
-
-
-// custom google map settings
-jQuery(document).ready(function($){
-	//set your google maps parameters
-	var latitude = 38.710202,
-		longitude = -9.142238,
-		map_zoom = 16;
-
-	//google map custom marker icon - .png fallback for IE11
-	var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
-	var marker_url = ( is_internetExplorer11 ) ? 'http://static.tumblr.com/1ccbc9v/cvXncd6ge/cd-icon-location.png' : 'http://static.tumblr.com/1ccbc9v/JWhncd6gz/cd-icon-location.svg';
-		
-	//define the basic color of your map, plus a value for saturation and brightness
-	var	main_color = '#2d313f',
-		saturation_value= -20,
-		brightness_value= 5;
-
-	//we define here the style of the map
-	var style= [ 
-		{
-			//set saturation for the labels on the map
-			elementType: "labels",
-			stylers: [
-				{saturation: saturation_value}
-			]
-		},  
-	    {	//poi stands for point of interest - don't show these lables on the map 
-			featureType: "poi",
-			elementType: "labels",
-			stylers: [
-				{visibility: "off"}
-			]
 		},
-		{
-			//don't show highways lables on the map
-	        featureType: 'road.highway',
-	        elementType: 'labels',
-	        stylers: [
-	            {visibility: "off"}
-	        ]
-	    }, 
-		{ 	
-			//don't show local road lables on the map
-			featureType: "road.local", 
-			elementType: "labels.icon", 
-			stylers: [
-				{visibility: "off"} 
-			] 
+		"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+			"enable": true,
+			"mode": "grab"
+			},
+			"onclick": {
+			"enable": true,
+			"mode": "push"
+			},
+			"resize": true
 		},
-		{ 
-			//don't show arterial road lables on the map
-			featureType: "road.arterial", 
-			elementType: "labels.icon", 
-			stylers: [
-				{visibility: "off"}
-			] 
-		},
-		{
-			//don't show road lables on the map
-			featureType: "road",
-			elementType: "geometry.stroke",
-			stylers: [
-				{visibility: "off"}
-			]
-		}, 
-		//style different elements on the map
-		{ 
-			featureType: "transit", 
-			elementType: "geometry.fill", 
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		}, 
-		{
-			featureType: "poi",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "poi.government",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "poi.sport_complex",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "poi.attraction",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "poi.business",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "transit",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "transit.station",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "landscape",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-			
-		},
-		{
-			featureType: "road",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "road.highway",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		}, 
-		{
-			featureType: "water",
-			elementType: "geometry",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
+		"modes": {
+			"grab": {
+			"distance": 400,
+			"line_linked": {
+				"opacity": 1
+			}
+			},
+			"bubble": {
+			"distance": 400,
+			"size": 40,
+			"duration": 2,
+			"opacity": 8,
+			"speed": 3
+			},
+			"repulse": {
+			"distance": 200,
+			"duration": 0.4
+			},
+			"push": {
+			"particles_nb": 4
+			},
+			"remove": {
+			"particles_nb": 2
+			}
 		}
-	];
-		
-	//set google map options
-	var map_options = {
-      	center: new google.maps.LatLng(latitude, longitude),
-      	zoom: map_zoom,
-      	panControl: false,
-      	zoomControl: false,
-      	mapTypeControl: false,
-      	streetViewControl: false,
-      	mapTypeId: google.maps.MapTypeId.ROADMAP,
-      	scrollwheel: false,
-      	styles: style,
-    }
-    //inizialize the map
-	var map = new google.maps.Map(document.getElementById('google-container'), map_options);
-	//add a custom marker to the map				
-	var marker = new google.maps.Marker({
-	  	position: new google.maps.LatLng(latitude, longitude),
-	    map: map,
-	    visible: true,
-	 	icon: marker_url,
+		},
+		"retina_detect": true
 	});
+};
 
-	//add custom buttons for the zoom-in/zoom-out on the map
-	function CustomZoomControl(controlDiv, map) {
-		//grap the zoom elements from the DOM and insert them in the map 
-	  	var controlUIzoomIn= document.getElementById('cd-zoom-in'),
-	  		controlUIzoomOut= document.getElementById('cd-zoom-out');
-	  	controlDiv.appendChild(controlUIzoomIn);
-	  	controlDiv.appendChild(controlUIzoomOut);
-
-		// Setup the click event listeners and zoom-in or out according to the clicked element
-		google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
-		    map.setZoom(map.getZoom()+1)
+function styleSpace(){
+	$('#particles-js').css("background-color", "rgb(35,39,65)");
+	particlesJS("particles-js", {
+		"particles": {
+			"number": {
+				"value": 160,
+				"density": {
+				"enable": true,
+				"value_area": 800
+				}
+			},
+			"color": {
+				"value": "#ffffff"
+			},
+			"shape": {
+				"type": "circle",
+				"stroke": {
+				"width": 0,
+				"color": "#000000"
+				},
+				"polygon": {
+				"nb_sides": 5
+				},
+				"image": {
+				"src": "img/github.svg",
+				"width": 100,
+				"height": 100
+				}
+			},
+			"opacity": {
+				"value": 1,
+				"random": true,
+				"anim": {
+				"enable": true,
+				"speed": 1,
+				"opacity_min": 0,
+				"sync": false
+				}
+			},
+			"size": {
+				"value": 3,
+				"random": true,
+				"anim": {
+				"enable": false,
+				"speed": 4,
+				"size_min": 0.3,
+				"sync": false
+				}
+			},
+			"line_linked": {
+				"enable": false,
+				"distance": 150,
+				"color": "#ffffff",
+				"opacity": 0.4,
+				"width": 1
+			},
+			"move": {
+				"enable": true,
+				"speed": 1,
+				"direction": "none",
+				"random": true,
+				"straight": false,
+				"out_mode": "out",
+				"bounce": false,
+				"attract": {
+				"enable": false,
+				"rotateX": 600,
+				"rotateY": 600
+				}
+			}
+			},
+			"interactivity": {
+			"detect_on": "canvas",
+			"events": {
+				"onhover": {
+				"enable": true,
+				"mode": "bubble"
+				},
+				"onclick": {
+				"enable": true,
+				"mode": "repulse"
+				},
+				"resize": true
+			},
+			"modes": {
+				"grab": {
+				"distance": 400,
+				"line_linked": {
+					"opacity": 1
+				}
+				},
+				"bubble": {
+				"distance": 250,
+				"size": 0,
+				"duration": 2,
+				"opacity": 0,
+				"speed": 3
+				},
+				"repulse": {
+				"distance": 400,
+				"duration": 0.4
+				},
+				"push": {
+				"particles_nb": 4
+				},
+				"remove": {
+				"particles_nb": 2
+				}
+			}
+		},
+		"retina_detect": true
 		});
-		google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
-		    map.setZoom(map.getZoom()-1)
-		});
-	}
+};
 
-	var zoomControlDiv = document.createElement('div');
- 	var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+function styleSnow(){
+	$('#particles-js').css("background-color", "rgb(100,149,237)");
+	particlesJS("particles-js", {
+		"particles": {
+		"number": {
+			"value": 400,
+			"density": {
+			"enable": true,
+			"value_area": 800
+			}
+		},
+		"color": {
+			"value": "#fff"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+			"width": 0,
+			"color": "#000000"
+			},
+			"polygon": {
+			"nb_sides": 5
+			},
+			"image": {
+			"src": "img/github.svg",
+			"width": 100,
+			"height": 100
+			}
+		},
+		"opacity": {
+			"value": 0.5,
+			"random": true,
+			"anim": {
+			"enable": false,
+			"speed": 1,
+			"opacity_min": 0.1,
+			"sync": false
+			}
+		},
+		"size": {
+			"value": 10,
+			"random": true,
+			"anim": {
+			"enable": false,
+			"speed": 40,
+			"size_min": 0.1,
+			"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": false,
+			"distance": 500,
+			"color": "#ffffff",
+			"opacity": 0.4,
+			"width": 2
+		},
+		"move": {
+			"enable": true,
+			"speed": 6,
+			"direction": "bottom",
+			"random": false,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+			"enable": false,
+			"rotateX": 600,
+			"rotateY": 1200
+			}
+		}
+		},
+		"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+			"enable": true,
+			"mode": "bubble"
+			},
+			"onclick": {
+			"enable": true,
+			"mode": "repulse"
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+			"distance": 400,
+			"line_linked": {
+				"opacity": 0.5
+			}
+			},
+			"bubble": {
+			"distance": 400,
+			"size": 4,
+			"duration": 0.3,
+			"opacity": 1,
+			"speed": 3
+			},
+			"repulse": {
+			"distance": 200,
+			"duration": 0.4
+			},
+			"push": {
+			"particles_nb": 4
+			},
+			"remove": {
+			"particles_nb": 2
+			}
+		}
+		},
+		"retina_detect": true
 
-  	//insert the zoom div on the top left of the map
-  	map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
-});
-
-  
+	});
+};
